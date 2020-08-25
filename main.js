2a5b24690aae513c77b4c362a9c8a1367951ba1f
NoelKling <chaoticforbidden@gmail.com>
2020-08-25 18:13:16 -0500

    created bombs that randomly generate

--- main.js
+++ main.js
@@ -2,12 +2,13 @@
 // Created on 8/19/2020
 // By Noel Kling
 
-let canvas;
-let col = 10;
 let ctx = null;
 let grid;
-let row = 10;
+let firstClick = true;
+let bombCount = 25;
 let size = 35;
+let col = 10;
+let row = 10;
 
 function twoDArray(col, row) {
   // Creates a 2D array
@@ -19,17 +20,25 @@ function twoDArray(col, row) {
 }
 
 function Setup() {
+  let canvas;
   // Setup canvas
   canvas = document.getElementById("gameBoard");
   ctx = canvas.getContext("2d");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  canvas.width = 401;
+  canvas.height = 401;
   // Set white background
   ctx.fillStyle = "#FFFFFF";
   ctx.fillRect(0, 0, canvas.width, canvas.width);
 
   // Scaled ctx to fit canvas frame
-  ctx.scale(2.75, 1.45);
+  ctx.scale(1.15, 1.15);
+
+  drawCell();
+  createBombs();
+}
+
+function drawCell() {
+  // Constructs the 2D array for the cells
   grid = twoDArray(col, row);
 
   // Create cells that hold the grid
@@ -39,14 +48,28 @@ function Setup() {
     }
   }
 
-  drawCell(col, row);
+  // Display the board
+  for (let i = 0; i < col; i++) {
+    for (let j = 0; j < row; j++) {
+      grid[i][j].show();
+    }
+  }
 }
 
-function drawCell(x, y) {
-  // Display the board
-  for (let i = 0; i < y; i++) {
-    for (let j = 0; j < x; j++) {
-      grid[i][j].show();
+function createBombs() {
+  while (bombCount != 0) {
+    let x = Math.floor(Math.random(0, col) * col);
+    let y = Math.floor(Math.random(0, row) * row);
+
+    if (!grid[x][y].isBomb) {
+      grid[x][y].isBomb = true;
+      bombCount--;
+    }
+  }
+
+  for (let i = 0; i < col; i++) {
+    for (let j = 0; j < row; j++) {
+      grid[i][j].bomb();
     }
   }
 }
