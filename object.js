75ef1c6994af0681d201fe23bfbdd8310e6ee99f
kenzieadmin <kenzieadmin@users.noreply.github.com>
2020-09-04 16:45:54 +0000

    deploy: b0d92622e19e7cf755f8d4956a56d5cc734155b5

--- object.js
+++ object.js
@@ -3,15 +3,15 @@
 // By Noel Kling
 
 class Cell {
-  constructor(i, j, size) {
+  constructor(i, j, sizeCell) {
     // Iterators
     this.i = i;
     this.j = j;
 
     // Grid based information
-    this.x = i * size;
-    this.y = j * size;
-    this.size = size;
+    this.x = i * sizeCell;
+    this.y = j * sizeCell;
+    this.size = sizeCell;
 
     // Bomb information
     this.isBomb = false;
@@ -20,14 +20,18 @@ class Cell {
   }
 
   show() {
-    // Constructs the visual grid based on the cells
-    ctx.strokeStyle = "#000000";
-    ctx.rect(this.x, this.y, this.size, this.size);
-    ctx.stroke();
+    this.cell();
     // Calls to draw the bombs on to the grid
     this.notBomb();
 
     this.bomb();
+  }
+
+  cell() {
+    // Constructs the visual grid based on the cells
+    ctx.strokeStyle = "#000000";
+    ctx.rect(this.x, this.y, this.size, this.size);
+    ctx.stroke();
   }
 
   bomb() {
@@ -36,7 +40,7 @@ class Cell {
       if (this.isBomb) {
         ctx.fillStyle = "black";
         ctx.font = "900 20px Arial";
-        ctx.fillText("♞", this.x + 5, this.y + this.size / 1.5);
+        ctx.fillText("♞", this.x + 10, this.y + this.size / 1.5);
       }
       ctx.fill();
       ctx.stroke();
@@ -73,7 +77,7 @@ class Cell {
       if (this.bombNeighbor > 0) {
         ctx.fillStyle = "black";
         ctx.font = "900 20px Arial";
-        ctx.fillText(this.bombNeighbor, this.x + 12, this.y + this.size / 1.5);
+        ctx.fillText(this.bombNeighbor, this.x + 15, this.y + this.size / 1.5);
       }
 
       ctx.stroke();
@@ -83,6 +87,31 @@ class Cell {
   reveal() {
     if (this.isRevealed === false) {
       this.isRevealed = true;
+    }
+    if (this.bombNeighbor === 0) {
+      this.fillFlood();
+    }
+  }
+
+  fillFlood() {
+    for (let seeX = -1; seeX <= 1; seeX++) {
+      for (let seeY = -1; seeY <= 1; seeY++) {
+        // Cycles through each cell in a 3x3 to determine if any edge/corner touch a bomb
+        let offSetX = this.i + seeX;
+        let offSetY = this.j + seeY;
+        if (
+          offSetX > -1 &&
+          offSetX < boardSize &&
+          offSetY > -1 &&
+          offSetY < boardSize
+        ) {
+          let cellFill = grid[offSetX][offSetY];
+          if (!cellFill.isBomb && !cellFill.isRevealed) {
+            cellFill.reveal();
+            cellFill.show();
+          }
+        }
+      }
     }
   }
 }
